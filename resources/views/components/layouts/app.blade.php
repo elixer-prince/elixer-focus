5008610544d05bdddf50deff4e39fef1275b707f
Daniel Leith <elixerprince@gmail.com>
2025-09-26 13:12:51 -0500

    feat: add a website favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,6 +10,7 @@
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ $pageTitle ?? config("app.name") }}</title>
+        <link rel="icon" href="{{ Vite::asset('resources/assets/images/elixerfocus-favicon.png') }}" />
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
 
