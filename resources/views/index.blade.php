ef11105dc1a1670764c7ac443bc5477846c311b6
Daniel Leith <elixerprince@gmail.com>
2025-09-27 12:47:00 -0500

    feat: add the white noise audio to the music list

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,19 @@
     <x-countdown-timer />
 
     <div>
+        <div class="font-bold">White Noise To Calm My Mind</div>
+
+        <audio controls loop>
+            <source
+                src="{{ Vite::asset("resources/assets/music/white-noise-for-studying.mp3") }}"
+                type="audio/mpeg"
+            />
+
+            <p>Your browser does not support the audio element.</p>
+        </audio>
+    </div>
+
+    <div>
         <div class="font-bold">90s Chill Lofi Playlist</div>
 
         <audio controls loop>
