80ecad293ef5dcb1ca5b607d4332edea121ff8d6
Daniel Leith <elixerprince@gmail.com>
2025-09-27 23:40:20 -0500

    refactor: extract a music-playlist component

---
+++ resources/views/components/music-playlist.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <div class="font-bold">White Noise To Calm My Mind</div>
+
+    <audio controls loop>
+        <source
+            src="{{ Vite::asset("resources/assets/music/white-noise-for-studying.mp3") }}"
+            type="audio/mpeg"
+        />
+
+        <p>Your browser does not support the audio element.</p>
+    </audio>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,44 +8,37 @@
 
     <x-countdown-timer />
 
-    <div>
-        <div class="font-bold">White Noise To Calm My Mind</div>
+    <x-music-playlist />
 
-        <audio controls loop>
-            <source
-                src="{{ Vite::asset("resources/assets/music/white-noise-for-studying.mp3") }}"
-                type="audio/mpeg"
-            />
-
-            <p>Your browser does not support the audio element.</p>
-        </audio>
-    </div>
-
-    <div>
+    {{--
+        <div>
         <div class="font-bold">90s Chill Lofi Playlist</div>
-
+        
         <audio controls loop>
-            <source
-                src="{{ Vite::asset("resources/assets/music/90s-chill-lofi-playlist-japanese-town.mp3") }}"
-                type="audio/mpeg"
-            />
-
-            <p>Your browser does not support the audio element.</p>
+        <source
+        src="{{ Vite::asset("resources/assets/music/90s-chill-lofi-playlist-japanese-town.mp3") }}"
+        type="audio/mpeg"
+        />
+        
+        <p>Your browser does not support the audio element.</p>
         </audio>
-    </div>
-
-    <div>
-        <div class="font-bold">
-            lofi hip hop mix 📚 beats to relax/study to (Part 1)
         </div>
+    --}}
 
+    {{--
+        <div>
+        <div class="font-bold">
+        lofi hip hop mix 📚 beats to relax/study to (Part 1)
+        </div>
+        
         <audio controls loop>
-            <source
-                src="{{ Vite::asset("resources/assets/music/lofi-girl-playlist.mp3") }}"
-                type="audio/mpeg"
-            />
-
-            <p>Your browser does not support the audio element.</p>
+        <source
+        src="{{ Vite::asset("resources/assets/music/lofi-girl-playlist.mp3") }}"
+        type="audio/mpeg"
+        />
+        
+        <p>Your browser does not support the audio element.</p>
         </audio>
-    </div>
+        </div>
+    --}}
 </x-layouts.app>
