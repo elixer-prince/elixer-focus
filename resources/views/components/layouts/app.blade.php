058ea2e09ba7afd363120d0f914a5b78e55bf9fb
Daniel Leith <elixerprince@gmail.com>
2025-09-29 00:13:55 -0500

    feat: add smooth scrolling to the app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+<html
+    lang="{{ str_replace("_", "-", app()->getLocale()) }}"
+    class="not-motion-reduce:scroll-smooth"
+>
     <head>
         <meta charset="utf-8" />
         <meta name="author" content="Elixer Prince" />
@@ -10,7 +13,10 @@
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ $pageTitle ?? config("app.name") }}</title>
-        <link rel="icon" href="{{ Vite::asset('resources/assets/images/elixerfocus-favicon.png') }}" />
+        <link
+            rel="icon"
+            href="{{ Vite::asset("resources/assets/images/elixerfocus-favicon.png") }}"
+        />
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
 
