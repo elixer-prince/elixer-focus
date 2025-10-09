876a2a983e33bcb710257a41d8b508a36a1c5d39
Daniel Leith <elixerprince@gmail.com>
2025-10-09 09:07:22 -0500

    chore: add comments for routes to be added

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view("/", "index");
+
+// TODO: Add Authentication Routes
+Route::prefix("auth")->group(function () {
+    // elixerfocus.com/auth/login
+    // elixerfocus.com/auth/register
+    // elixerfocus.com/auth/admin/login
+});
+
+// TODO: Add Settings Routes
+Route::prefix("settings")->group(function () {
+    // elixerfocus.com/settings/general
+    // elixerfocus.com/settings/timer
+});
