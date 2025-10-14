d0e0f993e7040c4d1f29171a5aa653dfcda9c432
Daniel Leith <elixerprince@gmail.com>
2025-10-14 13:32:13 -0500

    feat: create a basic hard coded tasks page

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,47 @@
+<x-layouts.app>
+    <p>
+        <a href="/" class="underline text-blue-500" wire:navigate>Back to home</a>
+    </p>
+
+    <div>
+        <div>
+            <h2 class="font-bold">Urgent and Important (Do now)</h2>
+
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+        </div>
+
+        <div>
+            <h2 class="font-bold">Not Urgent but Important (Plan/Schedule)</h2>
+
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+        </div>
+
+        <div>
+            <h2 class="font-bold">Urgent but Not Important (Delegate)</h2>
+
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+        </div>
+
+        <div>
+            <h2 class="font-bold">Not Urgent NOR Important (Delete)</h2>
+
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+        </div>
+    </div>
+</x-layouts.app>
