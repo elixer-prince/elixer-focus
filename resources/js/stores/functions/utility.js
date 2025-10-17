b717a27f645f6c00607db016cc3e8db202d1d258
Daniel Leith <elixerprince@gmail.com>
2025-10-17 13:05:49 -0500

    chore: remove unnecessary jsdoc comments

--- resources/js/stores/functions/utility.js
+++ resources/js/stores/functions/utility.js
@@ -1,29 +1,13 @@
 document.addEventListener("alpine:init", () => {
-   Alpine.store("utilityFunctions", {
-       /**
-        * Plays the specified sound.
-        *
-        * This method takes the URL path of a sound and plays it from
-        * the beginning.
-        *
-        * @param effect - The sound effect to be played.
-        */
-       playSound(effect) {
-           effect.currentTime = 0;
-           effect.play();
-       },
+    Alpine.store("utilityFunctions", {
+        playSound(effect) {
+            effect.currentTime = 0;
+            effect.play();
+        },
 
-       /**
-        * Stops the specified sound.
-        *
-        * This functions pauses the specified sound effect and reset the
-        * current time to zero to simulate stopping.
-        *
-        * @param effect - The sound effect to be stopped.
-        */
-       stopSound(effect) {
-           effect.pause();
-           effect.currentTime = 0;
-       },
-   });
+        stopSound(effect) {
+            effect.pause();
+            effect.currentTime = 0;
+        },
+    });
 });
