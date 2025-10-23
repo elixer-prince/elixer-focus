fbfa745011dc842681cc4e68615ed3cdd4ecf8ab
Daniel Leith <elixerprince@gmail.com>
2025-10-23 00:19:06 -0500

    feat(countdown-timer): add header session type buttons for session switching

--- react-frontend/src/components/CountdownTimer.tsx
+++ react-frontend/src/components/CountdownTimer.tsx
@@ -18,6 +18,30 @@ const CountdownTimer = () => {
                     <h2 className={"text-2xl font-black"}>
                         {currentSessionType} Session
                     </h2>
+
+                    <div className={"flex gap-4"}>
+                        <button
+                            className={
+                                "cursor-pointer rounded-md border-2 border-neutral-950 px-2 py-1 text-sm font-bold text-neutral-950 transition-colors"
+                            }
+                        >
+                            Focus
+                        </button>
+                        <button
+                            className={
+                                "cursor-pointer rounded-md border-2 border-neutral-950 px-2 py-1 text-sm font-bold text-neutral-950 transition-colors"
+                            }
+                        >
+                            Short Break
+                        </button>
+                        <button
+                            className={
+                                "cursor-pointer rounded-md border-2 border-neutral-950 px-2 py-1 text-sm font-bold text-neutral-950 transition-colors"
+                            }
+                        >
+                            Long Break
+                        </button>
+                    </div>
                 </div>
                 <div className={"font-bold"}>
                     Current Session: {currentSessionCount} / {sessionCountLimit}{" "}
