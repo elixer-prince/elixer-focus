ec6458fac4fb092714708a7784df685a9867b6b7
Daniel Leith <elixerprince@gmail.com>
2025-10-30 15:55:56 -0500

    feat: create a function to start the timer

--- react-frontend/src/components/CountdownTimer.tsx
+++ react-frontend/src/components/CountdownTimer.tsx
@@ -184,6 +184,52 @@ const CountdownTimer = () => {
         return Date.now() + remainingTimeInSeconds * 1000;
     }
 
+    function startTimer() {
+        if (!timerPaused) return;
+
+        playSound(onClickSoundEffect);
+
+        const newEndTime = calculateEndTime(remainingTimeInSeconds);
+
+        setEndTime(newEndTime);
+        saveToLocalStorage("endTime", newEndTime);
+
+        setTimerRunning(true);
+        setTimerPaused(false);
+
+        const interval = setInterval(() => {
+            setRemainingTimeInSeconds((currentRemainingTime: number) => {
+                if (currentRemainingTime === 0) {
+                    clearInterval(interval);
+                    setTimerRunning(false);
+                    setTimerPaused(true);
+                    return 0;
+                }
+
+                const newSeconds = Math.max(
+                    0,
+                    Math.round((newEndTime - Date.now()) / 1000),
+                );
+
+                saveToLocalStorage("remainingTimeInSeconds", newSeconds);
+
+                if (newSeconds <= 5) playSound(tickingSoundEffect);
+
+                if (newSeconds === 0) {
+                    stopSound(tickingSoundEffect);
+                    playSound(beepSoundEffect);
+                    clearInterval(interval);
+                    setTimerRunning(false);
+                    setTimerPaused(true);
+                    handleSessionTypeSwitch();
+                    setTimeout(() => alert("The timer has ended!"), 1000);
+                }
+
+                return newSeconds;
+            });
+        }, 1000);
+
+        setTimerInterval(interval);
     }
 
     function pauseTimer() {
