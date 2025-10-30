ed4304d55c7053e04c98e3cbb17391ad618c9a72
Daniel Leith <elixerprince@gmail.com>
2025-10-30 16:29:03 -0500

    chore: move speed insights

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <App />
-            <SpeedInsights />
         </BrowserRouter>
+        <SpeedInsights />
     </StrictMode>,
 );
