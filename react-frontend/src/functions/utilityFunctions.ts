a9aed39b4292370d73911b0d65edccd3ee7b87ca
Daniel Leith <elixerprince@gmail.com>
2025-10-30 07:58:28 -0500

    feat: create a helper function for saving to local storage

--- react-frontend/src/functions/utilityFunctions.ts
+++ react-frontend/src/functions/utilityFunctions.ts
@@ -2,6 +2,10 @@ export function getFromLocalStorage(key: string) {
     return JSON.parse(localStorage.getItem(key) as string);
 }
 
+export function saveToLocalStorage(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
 /*
 |----------------------------------------------------------------
 |  SOUND FUNCTIONS
