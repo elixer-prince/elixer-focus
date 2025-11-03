e95771ff98c74e4470c4b3af6967e325f6c19d31
Daniel Leith <elixerprince@gmail.com>
2025-11-03 00:02:34 -0500

    style(timer-display): increase the duration of the hover to make it more subtle

--- react-frontend/src/components/TimerDisplay.tsx
+++ react-frontend/src/components/TimerDisplay.tsx
@@ -12,7 +12,7 @@ const TimerDisplay = ({ startTimeInMinutes }: TimerDisplayProps) => {
     const remainingTimeInSeconds = startTimeInSeconds;
 
     return (
-        <div className="flex aspect-square items-center justify-center overflow-hidden rounded-full border-8 border-yellow-500 p-8 text-7xl font-bold transition-all duration-500 select-none hover:bg-neutral-800">
+        <div className="flex aspect-square items-center justify-center overflow-hidden rounded-full border-8 border-yellow-500 p-8 text-7xl font-bold transition-all duration-1000 select-none hover:bg-neutral-800">
             {formatTimeInMinutesAndSeconds(remainingTimeInSeconds)}
         </div>
     );
