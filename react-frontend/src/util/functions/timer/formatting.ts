21dc4fc065cfb486b864c707173506eaa8c3b72c
Daniel Leith <elixerprince@gmail.com>
2025-11-03 09:48:34 -0500

    refactor(utils): create a dedicated file for timer formatting

---
+++ react-frontend/src/util/functions/timer/formatting.ts
@@ -0,0 +1,5 @@
+export const formatTimeInMinutesAndSeconds = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsRemainder = seconds % 60;
+    return `${minutes.toString().padStart(2, "0")}:${secondsRemainder.toString().padStart(2, "0")}`;
+};
