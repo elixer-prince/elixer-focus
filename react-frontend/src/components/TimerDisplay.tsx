4cc1ecc1718c55987b908a1ba82b0f7f8a188531
Daniel Leith <elixerprince@gmail.com>
2025-11-03 03:15:50 -0500

    feat(timer-display): implement a simple timer start and pause

--- react-frontend/src/components/TimerDisplay.tsx
+++ react-frontend/src/components/TimerDisplay.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
     convertMinutesToSeconds,
     formatTimeInMinutesAndSeconds,
@@ -16,14 +16,45 @@ const TimerDisplay = ({ startTimeInMinutes }: TimerDisplayProps) => {
     const onClickSoundEffect = useRef(new Audio(onClickSoundURL));
     const offClickSoundEffect = useRef(new Audio(offClickSoundURL));
     const beepSoundEffect = useRef(new Audio(beepSoundURL));
+    const startTimeInSeconds = useRef(
+        convertMinutesToSeconds(startTimeInMinutes),
+    );
     const [remainingTimeInSeconds, setRemainingTimeInSeconds] = useState(
         convertMinutesToSeconds(startTimeInMinutes),
     );
+    const [timerRunning, setTimerRunning] = useState(false);
+
+    const interval = useRef<ReturnType<typeof setInterval> | null>(null);
+
+    useEffect(() => {
+        if (remainingTimeInSeconds <= 0) {
+            playSound(beepSoundEffect.current);
+            if (interval.current) clearInterval(interval.current);
+            setRemainingTimeInSeconds(startTimeInSeconds.current);
+        }
+    }, [remainingTimeInSeconds]);
+
+    const handleTimerStart = () => {
+        if (timerRunning) {
+            playSound(offClickSoundEffect.current);
+            if (interval.current) clearInterval(interval.current);
+            return setTimerRunning(false);
+        }
+
+        playSound(onClickSoundEffect.current);
+        setTimerRunning(true);
+        interval.current = setInterval(() => {
+            setRemainingTimeInSeconds((prevTime) => prevTime - 1);
+        }, 1000);
+    };
 
     return (
-        <div className="flex aspect-square items-center justify-center overflow-hidden rounded-full border-8 border-yellow-500 p-8 text-7xl font-bold transition-all duration-1000 select-none hover:bg-neutral-800">
+        <button
+            className="flex aspect-square items-center justify-center overflow-hidden rounded-full border-8 border-yellow-500 p-8 text-7xl font-bold transition-all duration-1000 select-none hover:bg-neutral-800 hover:duration-1000 active:bg-neutral-700 active:duration-100"
+            onClick={handleTimerStart}
+        >
             {formatTimeInMinutesAndSeconds(remainingTimeInSeconds)}
-        </div>
+        </button>
     );
 };
 
