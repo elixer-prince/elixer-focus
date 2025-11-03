be94c685f90d5d94c7abdd5d592344e010749dba
Daniel Leith <elixerprince@gmail.com>
2025-11-03 16:38:08 -0500

    chore(app): remove commented components

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,22 +1,11 @@
 import { Route, Routes } from "react-router";
-// COMPONENTS
-// import Navbar from "./components/Navbar.tsx";
-// PAGES
 import HomePage from "./pages/HomePage.tsx";
-// import TasksPage from "./pages/TasksPage.tsx";
-// import SettingsPage from "./pages/SettingsPage.tsx";
 
 const App = () => {
     return (
-        <>
-            {/* <Navbar appName={appName} /> */}
-            <Routes>
-                <Route path={"/"} element={<HomePage />} />
-                {/* <Route path={"/tasks"} element={<TasksPage />} /> */}
-                {/* <Route path={"/settings"} element={<SettingsPage />} /> */}
-            </Routes>
-            {/*<MusicPlayer />*/}
-        </>
+        <Routes>
+            <Route path={"/"} element={<HomePage />} />
+        </Routes>
     );
 };
 
