fc43cd939b391f02a4a2480396e90a275b661c64
Daniel Leith <elixerprince@gmail.com>
2025-11-02 23:09:54 -0500

    feat(timer-display): create a variable to store the remainingTimeInSeconds

--- react-frontend/src/components/TimerDisplay.tsx
+++ react-frontend/src/components/TimerDisplay.tsx
@@ -9,10 +9,11 @@ interface TimerDisplayProps {
 
 const TimerDisplay = ({ startTimeInMinutes }: TimerDisplayProps) => {
     const startTimeInSeconds = convertMinutesToSeconds(startTimeInMinutes);
+    const remainingTimeInSeconds = startTimeInSeconds;
 
     return (
-        <div className="flex aspect-square w-full max-w-full items-center justify-center rounded-full border-2 p-20 text-7xl font-bold select-none">
-            {formatTimeInMinutesAndSeconds(startTimeInSeconds)}
+        <div className="flex aspect-square w-full max-w-full items-center justify-center overflow-hidden rounded-full border-3 p-20 text-7xl font-bold select-none">
+            {formatTimeInMinutesAndSeconds(remainingTimeInSeconds)}
         </div>
     );
 };
