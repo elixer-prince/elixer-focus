f0fd5c8c4d99d8f26e9efb8f707902d9e96ffa26
Daniel Leith <elixerprince@gmail.com>
2025-11-04 17:38:18 -0500

    refactor(utils): modify the save to localstorage function to use generics instead of unknown

--- react-frontend/src/util/functions/storage.ts
+++ react-frontend/src/util/functions/storage.ts
@@ -2,6 +2,6 @@ export function getFromLocalStorage(key: string) {
     return JSON.parse(localStorage.getItem(key) as string);
 }
 
-export function saveToLocalStorage(key: string, value: unknown): void {
+export function saveToLocalStorage<Type>(key: string, value: Type): void {
     localStorage.setItem(key, JSON.stringify(value));
 }
