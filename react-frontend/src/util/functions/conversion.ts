30e223b5eb4f8edc51391e792006f534dfd795cc
Daniel Leith <elixerprince@gmail.com>
2025-11-04 10:25:36 -0500

    feat(utils): create a function to convert minutes to milliseconds

--- react-frontend/src/util/functions/conversion.ts
+++ react-frontend/src/util/functions/conversion.ts
@@ -1,3 +1,7 @@
 export const convertMinutesToSeconds = (minutes: number) => {
     return minutes * 60;
 };
+
+export const convertMinutesToMilliseconds = (minutes: number) => {
+    return convertMinutesToSeconds(minutes) * 1000;
+};
