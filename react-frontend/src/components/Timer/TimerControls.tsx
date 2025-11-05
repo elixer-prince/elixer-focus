f9c601cd09e2d0304c97136e8d8be35985d7ecc9
Daniel Leith <elixerprince@gmail.com>
2025-11-05 03:06:05 -0500

    feat(timer-controls): add four basic buttons to be modified later

--- react-frontend/src/components/Timer/TimerControls.tsx
+++ react-frontend/src/components/Timer/TimerControls.tsx
@@ -1,5 +1,12 @@
 const TimerControls = () => {
-    return <div>(Timer Controls)</div>;
+    return (
+        <div className="flex justify-center gap-2">
+            <button className="btn rounded-md">Play</button>
+            <button className="btn rounded-md">Pause</button>
+            <button className="btn rounded-md">Reset</button>
+            <button className="btn rounded-md">Skip</button>
+        </div>
+    );
 };
 
 export default TimerControls;
