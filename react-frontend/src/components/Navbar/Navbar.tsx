44ee70a9de5705046a35931ad051c188253948dc
Daniel Leith <elixerprince@gmail.com>
2025-11-05 10:37:47 -0500

    style(navbar): add the same transition to navbar colours ass the body

--- react-frontend/src/components/Navbar/Navbar.tsx
+++ react-frontend/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 const Navbar = () => {
     return (
-        <nav className="bg-base-300 sticky top-0 right-0 left-0 flex items-center justify-between p-4 pl-8">
+        <nav className="bg-base-300 sticky top-0 right-0 left-0 flex items-center justify-between p-4 pl-8 transition-colors duration-1000">
             <ul>
                 <li>
                     <Link className={"text-primary hover:underline"} to={"/"}>
