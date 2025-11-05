5981591470fc89a56d70f168d3910cca428fdad7
Daniel Leith <elixerprince@gmail.com>
2025-11-05 10:22:32 -0500

    style(settings-page): add a settings page

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.tsx";
+import SettingsPage from "./pages/SettingsPage.tsx";
 
 const App = () => {
     return (
         <Routes>
             <Route index element={<HomePage />} />
+            <Route path="settings" element={<SettingsPage />} />
         </Routes>
     );
 };
--- react-frontend/src/pages/SettingsPage.tsx
+++ react-frontend/src/pages/SettingsPage.tsx
@@ -3,7 +3,62 @@ import AppLayout from "../components/layouts/AppLayout";
 const SettingsPage = () => {
     return (
         <AppLayout>
-            <h1 className={"font-bold"}>Settings Page</h1>
+            <fieldset className="fieldset">
+                <label className="flex cursor-pointer items-center gap-2">
+                    <input
+                        type="radio"
+                        name="theme-radios"
+                        className="radio radio-sm theme-controller"
+                        value="default"
+                    />
+                    Default
+                </label>
+                <label className="flex cursor-pointer items-center gap-2">
+                    <input
+                        type="radio"
+                        name="theme-radios"
+                        className="radio radio-sm theme-controller"
+                        value="luxury"
+                    />
+                    Luxury
+                </label>
+                <label className="flex cursor-pointer items-center gap-2">
+                    <input
+                        type="radio"
+                        name="theme-radios"
+                        className="radio radio-sm theme-controller"
+                        value="retro"
+                    />
+                    Retro
+                </label>
+                <label className="flex cursor-pointer items-center gap-2">
+                    <input
+                        type="radio"
+                        name="theme-radios"
+                        className="radio radio-sm theme-controller"
+                        value="cyberpunk"
+                    />
+                    Cyberpunk
+                </label>
+                <label className="flex cursor-pointer items-center gap-2">
+                    <input
+                        type="radio"
+                        name="theme-radios"
+                        className="radio radio-sm theme-controller"
+                        value="valentine"
+                    />
+                    Valentine
+                </label>
+                <label className="flex cursor-pointer items-center gap-2">
+                    <input
+                        type="radio"
+                        name="theme-radios"
+                        className="radio radio-sm theme-controller"
+                        value="aqua"
+                    />
+                    Aqua
+                </label>
+            </fieldset>
         </AppLayout>
     );
 };
