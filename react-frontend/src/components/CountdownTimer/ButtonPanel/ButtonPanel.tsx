727fc4c6c5d960622adefcc1a103bb0701f9458e
Daniel Leith <elixerprince@gmail.com>
2025-11-06 09:51:09 -0500

    feat(countdown-timer): conditionally show the start or pause button

--- react-frontend/src/components/CountdownTimer/ButtonPanel/ButtonPanel.tsx
+++ react-frontend/src/components/CountdownTimer/ButtonPanel/ButtonPanel.tsx
@@ -2,12 +2,16 @@ import PauseButton from "./PauseButton";
 import StartButton from "./StartButton";
 import ResetButton from "./ResetButton";
 import SkipButton from "./SkipButton";
+import useCountdownTimerChecks from "../../../hooks/CountdownTimer/useCountdownTimerChecks.tsx";
 
 const CountdownButtonControls = () => {
+    const { countdownTimerIsPaused, countdownTimerIsNotPaused } =
+        useCountdownTimerChecks();
+
     return (
         <div className="flex justify-center gap-2">
-            <StartButton />
-            <PauseButton />
+            {countdownTimerIsPaused() && <StartButton />}
+            {countdownTimerIsNotPaused() && <PauseButton />}
             <ResetButton />
             <SkipButton />
         </div>
