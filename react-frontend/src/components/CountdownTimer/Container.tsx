bf415e513d0e75e7f0c9c038d87079847972df5c
Daniel Leith <elixerprince@gmail.com>
2025-11-07 18:53:32 -0500

    refactor(timer-display): extract the header into a dedicated component

--- react-frontend/src/components/CountdownTimer/Container.tsx
+++ react-frontend/src/components/CountdownTimer/Container.tsx
@@ -2,22 +2,14 @@ import { CountdownTimerProvider } from "../../contexts/CountdownTimerContext";
 import { CountdownSessionProvider } from "../../contexts/CountdownSessionContext";
 import CountdownButtonControls from "./ButtonPanel/ButtonPanel";
 import CountdownDisplay from "./Display";
+import Header from "./Header.tsx";
 
 const CountdownTimerContainer = () => {
     return (
         <CountdownTimerProvider>
             <CountdownSessionProvider>
                 <section className="flex flex-col items-center justify-center gap-8 p-4">
-                    {/* <CountdownTimerHeader
-                currentSessionType={currentSessionType}
-                setCurrentSessionType={setCurrentSessionType}
-                currentSessionCount={currentSessionCount}
-                sessionCountLimit={sessionCountLimit}
-                totalSessionsCompleted={totalSessionsCompleted}
-            /> */}
-                    <header>
-                        <p className="text-4xl font-bold">Focus Session</p>
-                    </header>
+                    <Header />
 
                     <CountdownDisplay />
 
