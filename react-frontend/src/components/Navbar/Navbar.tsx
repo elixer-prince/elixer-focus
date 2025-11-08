dd148cfee72a85858b5ffaf708371670aa9e159f
Daniel Leith <elixerprince@gmail.com>
2025-11-07 19:03:59 -0500

    feat(navbar): move home link to logo and add a tasks page link

--- react-frontend/src/components/Navbar/Navbar.tsx
+++ react-frontend/src/components/Navbar/Navbar.tsx
@@ -2,20 +2,27 @@ import { Link } from "react-router";
 
 const Navbar = () => {
     return (
-        <nav className="bg-base-300 sticky top-0 right-0 left-0 flex items-center justify-between p-4 pl-8 transition-colors duration-1000">
-            <ul>
-                <li>
-                    <Link className={"text-primary hover:underline"} to={"/"}>
-                        Home
+        <nav className="bg-base-300 sticky top-0 right-0 left-0 z-20 flex items-center justify-between p-4 pl-8 transition-colors duration-1000">
+            <div className="flex items-baseline gap-8">
+                <div>
+                    <Link to="/">
+                        <span className="text-xl font-bold">Elixer Focus</span>
                     </Link>
-                </li>
-            </ul>
+                </div>
+                <ul>
+                    <li>
+                        <Link
+                            className={"text-primary hover:underline"}
+                            to={"/tasks"}
+                        >
+                            Tasks
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
             <div>
-                <Link
-                    className={"btn btn-soft btn-primary"}
-                    to={"/settings"}
-                >
+                <Link className={"btn btn-soft btn-primary"} to={"/settings"}>
                     Settings
                 </Link>
             </div>
