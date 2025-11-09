1ed0702a308c0fe2f92448448c0fbcda1403fd46
Daniel Leith <elixerprince@gmail.com>
2025-11-09 09:27:35 -0500

    feat(app): add tasks route

--- react-frontend/src/app/router.tsx
+++ react-frontend/src/app/router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "@app/routes/Home";
 import Settings from "@app/routes/Settings";
+import Tasks from "@app/routes/Tasks.tsx";
 
 const Router = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
+            <Route path="tasks" element={<Tasks />} />
             <Route path="settings" element={<Settings />} />
         </Routes>
     );
