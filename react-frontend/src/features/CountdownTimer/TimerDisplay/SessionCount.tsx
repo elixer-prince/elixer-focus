6d7385da44a05f393ffee60076c727394a9e5698
Daniel Leith <elixerprince@gmail.com>
2025-12-08 05:23:15 -0500

    style(countdown-timer): add subtle gow to session count display on hover

--- react-frontend/src/features/CountdownTimer/TimerDisplay/SessionCount.tsx
+++ react-frontend/src/features/CountdownTimer/TimerDisplay/SessionCount.tsx
@@ -4,7 +4,7 @@ const SessionCount = () => {
     const { currentSessionCount, sessionCountLimit } = useSessionContext();
 
     return (
-        <div className="bg-base-100 border-primary/25 border-t-primary group-hover:border-primary/55 group-hover:border-t-primary absolute top-12 rounded-md border px-2 py-1 font-bold transition-colors duration-1000">
+        <div className="bg-base-100 border-primary/25 border-t-primary group-hover:border-primary/55 group-hover:border-t-primary group-hover:shadow-primary/25 absolute top-12 rounded-md border px-2 py-1 font-bold transition-all duration-1000 group-hover:shadow-[0_0_18px_var(--p)]">
             {currentSessionCount} <span className="text-primary">/</span>{" "}
             {sessionCountLimit}
         </div>
