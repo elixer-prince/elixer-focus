29ce4709adfbbea7135101282bc4b7dd4ba00ce5
Daniel Leith <elixerprince@gmail.com>
2025-12-17 18:16:53 -0500

    docs(footer): add comment to make the components clearer

--- react-frontend/src/features/ui/Footer/Index.tsx
+++ react-frontend/src/features/ui/Footer/Index.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
     return (
+        // Footer
         <footer className="px-8 py-4">
+            {/* Copyright Info */}
             <p className="text-center">Elixer Focus &copy; 2025</p>
         </footer>
     );
