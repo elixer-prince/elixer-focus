95f004e2c9419ffc49c06fd42b2b9d1bf18e8177
Daniel Leith <elixerprince@gmail.com>
2025-12-20 13:27:49 -0500

    feat(navbar): add navbar functionality

--- react-frontend/src/features/ui/Navigation/Navbar/Index.tsx
+++ react-frontend/src/features/ui/Navigation/Navbar/Index.tsx
@@ -1,12 +1,12 @@
-import ProfileAvatar from "@features/ui/Navigation/Navbar/ProfileAvatar.tsx";
+import { NavbarProvider } from "@features/ui/Navigation/Navbar/stores/NavbarContext.tsx";
+import useNavbarContext from "@features/ui/Navigation/Navbar/hooks/useNavbarContext.tsx";
+import HamburgerMenu from "@features/ui/Navigation/Navbar/HamburgerMenu.tsx";
 import NavLogo from "@features/ui/Navigation/NavLogo.tsx";
 import NavLinkContainer from "@features/ui/Navigation/Navbar/NavLinkContainer.tsx";
-import HamburgerMenu from "@features/ui/Navigation/Navbar/HamburgerMenu.tsx";
 import BrainDumpIcon from "@features/ui/Navigation/Navbar/BrainDumpIcon.tsx";
 import SettingsIcon from "@features/ui/Navigation/Navbar/SettingsIcon.tsx";
 import LoginLink from "@features/ui/Navigation/Navbar/LoginLink.tsx";
-import { NavbarProvider } from "@features/ui/Navigation/Navbar/stores/NavbarContext.tsx";
-import useNavbarContext from "@features/ui/Navigation/Navbar/hooks/useNavbarContext.tsx";
+import ProfileAvatar from "@features/ui/Navigation/Navbar/ProfileAvatar.tsx";
 
 const NavbarContent = () => {
     const { isOpen } = useNavbarContext();
@@ -14,24 +14,27 @@ const NavbarContent = () => {
     return (
         // Navbar Content
         <nav
-            className={`${isOpen ? "" : ""} bg-base-100/50 border-b-base-content/25 fixed top-0 right-0 left-0 z-20 flex h-(--navbar-height) items-center justify-between border-b p-4 backdrop-blur-xs transition-colors duration-1000`.trim()}
+            className={`${isOpen ? "bg-base-300 h-screen" : ""} bg-base-100/50 border-b-base-content/25 fixed top-0 right-0 left-0 z-20 h-(--navbar-height) border-b p-4 backdrop-blur-xs transition-all duration-1000`}
         >
-            {/* Left Container */}
-            <div className="flex items-center gap-4">
-                <HamburgerMenu />
+            {/* Inner Navbar Container */}
+            <div className="flex items-center justify-between">
+                {/* Left Container */}
+                <div className="flex items-center gap-4">
+                    <HamburgerMenu />
 
-                <div className="flex items-center gap-8">
-                    <NavLogo />
-                    <NavLinkContainer />
+                    <div className="flex items-center gap-8">
+                        <NavLogo />
+                        <NavLinkContainer />
+                    </div>
                 </div>
-            </div>
 
-            {/* Right Container */}
-            <div className="flex items-center gap-4">
-                <BrainDumpIcon />
-                <SettingsIcon />
-                <LoginLink />
-                <ProfileAvatar />
+                {/* Right Container */}
+                <div className="flex items-center gap-4">
+                    <BrainDumpIcon />
+                    <SettingsIcon />
+                    <LoginLink />
+                    <ProfileAvatar />
+                </div>
             </div>
         </nav>
     );
