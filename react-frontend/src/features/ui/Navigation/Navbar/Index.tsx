ab83f154f1a6e5db295a933f6ad8636718a3c97f
Daniel Leith <elixerprince@gmail.com>
2025-12-19 20:44:38 -0500

    fix(navbar): remove test div

--- react-frontend/src/features/ui/Navigation/Navbar/Index.tsx
+++ react-frontend/src/features/ui/Navigation/Navbar/Index.tsx
@@ -16,7 +16,6 @@ const NavbarContent = () => {
         <nav
             className={`${isOpen ? "bg-red-500" : ""} fixed top-0 right-0 left-0 z-20 flex h-(--navbar-height) items-center justify-between border-b px-8 py-4 backdrop-blur-xs transition-colors duration-1000`.trim()}
         >
-            <div>State: {String(isOpen)}</div>
             {/* Left Container */}
             <div className="flex items-center gap-4">
                 <HamburgerMenu />
