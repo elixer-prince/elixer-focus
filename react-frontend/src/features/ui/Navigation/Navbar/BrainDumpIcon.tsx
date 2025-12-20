d0f06901d94517653ea43c1262eca00aa6f67a53
Daniel Leith <elixerprince@gmail.com>
2025-12-20 13:27:07 -0500

    style(navbar): add hover style to brain dump icon

--- react-frontend/src/features/ui/Navigation/Navbar/BrainDumpIcon.tsx
+++ react-frontend/src/features/ui/Navigation/Navbar/BrainDumpIcon.tsx
@@ -3,7 +3,10 @@ import { FiInbox } from "react-icons/fi";
 
 const BrainDumpIcon = () => {
     return (
-        <Link to="/bin" className="">
+        <Link
+            to="/bin"
+            className="hover:text-primary transition-colors duration-300"
+        >
             <FiInbox size={24} />
         </Link>
     );
