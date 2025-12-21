4dfad1a5c458dbeadf7b5840d6cfd5914c34a7c9
Daniel Leith <elixerprince@gmail.com>
2025-12-21 08:11:54 -0500

    style(navbar): makes settings link visible

--- react-frontend/src/features/ui/Navigation/Navbar/SettingsIcon.tsx
+++ react-frontend/src/features/ui/Navigation/Navbar/SettingsIcon.tsx
@@ -1,11 +1,12 @@
-import { FiSettings } from "react-icons/fi";
-import { Link } from "react-router";
+import NavLink from "@features/ui/Navigation/Navbar/NavLink.tsx";
 
 const SettingsIcon = () => {
     return (
-        <Link to="/settings/themes" className={"hidden"}>
-            <FiSettings size={24} />
-        </Link>
+        // <Link to="/settings/themes" className={"hidden"}>
+        //     {/*<FiSettings size={24} />*/}
+        // </Link>
+
+        <NavLink to="/settings/timer">Settings</NavLink>
     );
 };
 
