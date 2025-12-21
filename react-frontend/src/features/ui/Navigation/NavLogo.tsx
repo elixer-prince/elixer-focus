5309956eca31df756e5264ad1ffbe1d4a832103a
Daniel Leith <elixerprince@gmail.com>
2025-12-21 07:51:45 -0500

    style(navbar): remove selection on site logo

--- react-frontend/src/features/ui/Navigation/NavLogo.tsx
+++ react-frontend/src/features/ui/Navigation/NavLogo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const NavLogo = () => {
     return (
         // Navbar Logo
-        <Link to="/">
+        <Link to="/" className="select-none">
             <span className="text-xl font-bold">Elixer Focus</span>
         </Link>
     );
