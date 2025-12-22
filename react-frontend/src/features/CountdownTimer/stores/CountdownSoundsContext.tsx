dc3464db2708f67c2960d9205cd8373387d003cb
Daniel Leith <elixerprince@gmail.com>
2025-12-22 07:45:21 -0500

    fix(countdown-timer): fix the session count hover effect and unused react import

--- react-frontend/src/features/CountdownTimer/stores/CountdownSoundsContext.tsx
+++ react-frontend/src/features/CountdownTimer/stores/CountdownSoundsContext.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const CountdownSoundsContext = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    return <div></div>;
 };
 
 export default CountdownSoundsContext;
