2ba9807853e8456a74e935d779fbde028391d63e
Daniel Leith <elixerprince@gmail.com>
2026-01-13 23:35:44 -0500

    feat(app): add vercel analytics to track site data

--- react-frontend/src/app/app.tsx
+++ react-frontend/src/app/app.tsx
@@ -1,6 +1,7 @@
 import "@app/index.css";
 import Provider from "@app/provider";
 import { router } from "@app/router";
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider>
             <RouterProvider router={router} />
+            <Analytics />
         </Provider>
     </StrictMode>,
 );
