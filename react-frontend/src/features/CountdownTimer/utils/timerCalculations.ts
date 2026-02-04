5a8fd572613bf7510ae05345cabd9f074908cacc
Daniel Leith <elixerprince@gmail.com>
2026-02-04 13:54:43 -0500

    docs(countdown-timer): add jsdoc comments to the utility functions

--- react-frontend/src/features/CountdownTimer/utils/timerCalculations.ts
+++ react-frontend/src/features/CountdownTimer/utils/timerCalculations.ts
@@ -1,13 +1,31 @@
 import { getCurrentTimestamp } from "@utils/date.ts";
 import { convertSecondsToMilliseconds } from "@utils/conversion.ts";
 
-export const calculateEndTime = (remainingTimeInSeconds: number) => {
+/**
+ * Calculates the countdown timer's end time based on the amount of seconds remaining.
+ *
+ * @param {number} remainingTimeInSeconds - The current remaining time in seconds.
+ * @returns {number} The current remaining time in milliseconds added to the current timestamp.
+ * */
+export const calculateEndTime = (remainingTimeInSeconds: number): number => {
     return (
         getCurrentTimestamp() +
         convertSecondsToMilliseconds(remainingTimeInSeconds)
     );
 };
 
-export const calculateRemainingSeconds = (now: number, endTime: number) => {
+/**
+ * Calculates the remaining time in seconds based on the timer's calculated end time.
+ *
+ * @param {number} now - The current timestamp in milliseconds.
+ * @param {number} endTime - The end time in milliseconds.
+ * @returns {number} The remaining time in seconds.
+ * */
+export const calculateRemainingSeconds = (
+    now: number,
+    endTime: number,
+): number => {
+    // Math.max() is used here to ensure the remaining seconds is never less
+    // than zero.
     return Math.max(0, Math.round((endTime - now) / 1000));
 };
--- react-frontend/src/features/CountdownTimer/utils/timerChecks.ts
+++ react-frontend/src/features/CountdownTimer/utils/timerChecks.ts
@@ -1,7 +1,21 @@
-export const timerIsAboutToEnd = (remainingSeconds: number) => {
+/**
+ * This checks if the timer is about to end by checking if the remaining
+ * seconds is less than 10.
+ *
+ * @param {number} remainingSeconds - The amount of seconds remaining.
+ * @returns {boolean} If the timer is about to end or not.
+ */
+export const timerIsAboutToEnd = (remainingSeconds: number): boolean => {
     return remainingSeconds <= 10;
 };
 
-export const timerHasEnded = (remainingSeconds: number) => {
+/**
+ * This checks if the timer has ended by checking if the remaining seconds is
+ * less than or equal to 0.
+ *
+ * @param {number} remainingSeconds - The amount of seconds remaining.
+ * @returns {boolean} If the timer is about to end or not.
+ */
+export const timerHasEnded = (remainingSeconds: number): boolean => {
     return remainingSeconds <= 0;
 };
