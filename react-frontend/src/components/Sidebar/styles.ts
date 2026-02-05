c244b74eb084e196914f1f0ec806ba7592a71ca1
Daniel Leith <elixerprince@gmail.com>
2026-02-04 23:45:18 -0500

    fix(sidebar): add missing export

--- react-frontend/src/components/Sidebar/styles.ts
+++ react-frontend/src/components/Sidebar/styles.ts
@@ -1,2 +1,5 @@
 export const sidebarStyles =
-    "border-base-content/50 fixed top-(--navbar-height) bottom-0 w-(--sidebar-width) border-r-2";
+    "border-base-content/50 flex flex-col fixed top-(--navbar-height) bottom-0 w-(--sidebar-width) border-r-2";
+
+export const navLinkStyles =
+    "hover:bg-base-content/25 block rounded-md border p-2 transition-colors";
