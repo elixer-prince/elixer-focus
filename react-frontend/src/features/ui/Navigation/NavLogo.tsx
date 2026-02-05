e3c838f7eeb75316220cc86ff1fa8c617a418680
Daniel Leith <elixerprince@gmail.com>
2026-02-05 17:34:49 -0500

    style(navbar): remove unnecessary hover effect from logo

--- react-frontend/src/features/ui/Navigation/NavLogo.tsx
+++ react-frontend/src/features/ui/Navigation/NavLogo.tsx
@@ -1,11 +1,7 @@
 const NavLogo = ({ className }: { className?: string }) => {
     return (
         <a href={"#"} className={`${className} select-none`.trim()}>
-            <span
-                className={
-                    "inline-block text-xl font-bold transition-transform duration-700 hover:-translate-y-0.25"
-                }
-            >
+            <span className={"inline-block text-xl font-bold"}>
                 Elixer Focus <sub className={"text-[0.5rem]"}>TM</sub>
             </span>
         </a>
