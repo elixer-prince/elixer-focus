19bb93e51a841bc18b931532c888b1f3550e1c32
Daniel Leith <elixerprince@gmail.com>
2026-02-05 16:42:30 -0500

    fix(navbar): remove missing import

--- react-frontend/src/components/Navbar/SettingsIcon.tsx
+++ react-frontend/src/components/Navbar/SettingsIcon.tsx
@@ -1,13 +1,10 @@
-import NavbarLink from "@components/Navbar/NavbarLink.tsx";
-
 const SettingsIcon = () => {
-    return (
-        // <Link to="/settings/themes" className={"hidden"}>
-        //     {/*<FiSettings size={24} />*/}
-        // </Link>
+    return null;
+    // <Link to="/settings/themes" className={"hidden"}>
+    //     {/*<FiSettings size={24} />*/}
+    // </Link>
 
-        <NavbarLink to="/settings/timer">Settings</NavbarLink>
-    );
+    // <NavbarLink to="/settings/timer">Settings</NavbarLink>
 };
 
 export default SettingsIcon;
