d98d56531569898b4c1e4254cf79950125cbd547
Daniel Leith <elixerprince@gmail.com>
2026-02-05 23:49:59 -0500

    style(home): modify main margin to only appear on md screens

--- react-frontend/src/app/layouts/Default.tsx
+++ react-frontend/src/app/layouts/Default.tsx
@@ -12,7 +12,7 @@ const DefaultLayout = () => {
 
             <main
                 className={
-                    "bg-base-300 mt-(--navbar-height) sm:ml-(--sidebar-width)"
+                    "bg-base-300 mt-(--navbar-height) md:ml-(--sidebar-width)"
                 }
             >
                 <Outlet />
