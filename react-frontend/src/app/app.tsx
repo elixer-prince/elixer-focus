215b14fe415ee7042f862885b582d6191d619859
Daniel Leith <elixerprince@gmail.com>
2026-02-07 20:52:19 -0500

    feat(app): implement global provider wrapper

--- react-frontend/src/app/app.tsx
+++ react-frontend/src/app/app.tsx
@@ -1,4 +1,5 @@
 import "@/app/index.css";
+import Provider from "@/app/provider";
 import { router } from "@/app/router";
 import { Analytics } from "@vercel/analytics/react";
 import gsap from "gsap";
@@ -11,7 +12,9 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Analytics />
+    <Provider>
+      <RouterProvider router={router} />
+      <Analytics />
+    </Provider>
   </StrictMode>,
 );
---
+++ react-frontend/src/app/provider.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from "react";
+
+const Provider = ({ children }: PropsWithChildren) => {
+  return <>{children}</>;
+};
+
+export default Provider;
