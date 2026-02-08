40f7a1e1842f8dc9bc3edf223b2d7b95a7023035
Daniel Leith <elixerprince@gmail.com>
2026-02-08 01:07:25 -0500

    refactor(countdown-timer): convert state to zustand store

--- react-frontend/src/features/CountdownTimer/components/SessionDisplay/Index.tsx
+++ react-frontend/src/features/CountdownTimer/components/SessionDisplay/Index.tsx
@@ -2,12 +2,14 @@ import CurrentSessionType from "@/features/CountdownTimer/components/SessionDisp
 import SessionSwitcher from "@/features/CountdownTimer/components/SessionDisplay/SessionSwitcher/Index";
 
 const CountdownTimerHeader = () => {
-  return (
-    <header className={"mt-8 flex flex-col items-center justify-center gap-4"}>
-      <CurrentSessionType />
-      <SessionSwitcher />
-    </header>
-  );
+    return (
+        <header
+            className={"mt-8 flex flex-col items-center justify-center gap-4"}
+        >
+            <CurrentSessionType />
+            <SessionSwitcher />
+        </header>
+    );
 };
 
 export default CountdownTimerHeader;
