6e311e2c6b331c811966fca97fc5e7286139b46a
Daniel Leith <elixerprince@gmail.com>
2026-02-08 13:07:10 -0500

    feat(router): uncomment signup route and remove all other comments

--- react-frontend/src/app/router.tsx
+++ react-frontend/src/app/router.tsx
@@ -1,11 +1,12 @@
 import AuthLayout from "@/app/layouts/Auth";
-import DefaultLayout from "@/app/layouts/Default.tsx";
-import Home from "@/app/routes/(ROOT)/Home.tsx";
-import Journal from "@/app/routes/(ROOT)/Journal.tsx";
-import Profile from "@/app/routes/(ROOT)/Profile.tsx";
-import Tasks from "@/app/routes/(ROOT)/Tasks.tsx";
+import DefaultLayout from "@/app/layouts/Default";
+import Home from "@/app/routes/(ROOT)/Home";
+import Journal from "@/app/routes/(ROOT)/Journal";
+import Profile from "@/app/routes/(ROOT)/Profile";
+import Tasks from "@/app/routes/(ROOT)/Tasks";
 import NotFound from "@/app/routes/404";
 import Login from "@/app/routes/Auth/Login";
+import SignUp from "@/app/routes/Auth/SignUp";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -30,30 +31,8 @@ export const router = createBrowserRouter([
         path: "tasks",
         element: <Tasks />,
       },
-      // {
-      //     path: "/calendar",
-      //     element: <Calendar />,
-      // },
-      // {
-      //     path: "/bin",
-      //     element: <Bin />,
-      // },
     ],
   },
-  // {
-  //     path: "/settings",
-  //     element: <Settings />,
-  //     children: [
-  //         {
-  //             path: "timer",
-  //             element: <TimerSettings />,
-  //         },
-  //         {
-  //             path: "themes",
-  //             element: <ThemeSettings />,
-  //         },
-  //     ],
-  // },
   {
     path: "/auth",
     element: <AuthLayout />,
@@ -62,32 +41,10 @@ export const router = createBrowserRouter([
         path: "login",
         element: <Login />,
       },
-      //         {
-      //             path: "/signup",
-      //             element: <SignUp />,
-      //         },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
     ],
   },
-  // {
-  //     path: "/onboarding",
-  //     element: <Onboarding />,
-  //     children: [
-  //         {
-  //             path: "step1",
-  //             element: <Step1 />,
-  //         },
-  //         {
-  //             path: "step2",
-  //             element: <Step2 />,
-  //         },
-  //         {
-  //             path: "step3",
-  //             element: <Step3 />,
-  //         },
-  //         {
-  //             path: "step4",
-  //             element: <Step4 />,
-  //         },
-  //     ],
-  // },
 ]);
