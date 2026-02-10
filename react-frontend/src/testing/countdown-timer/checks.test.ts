a83d08a8a37c64b6ff029a8914b070642f615b85
Daniel Leith <elixerprince@gmail.com>
2026-02-09 22:08:26 -0500

    test(utils): convert description to use words instead of expressions

--- react-frontend/src/testing/countdown-timer/checks.test.ts
+++ react-frontend/src/testing/countdown-timer/checks.test.ts
@@ -5,29 +5,29 @@ import {
 import { describe, expect, it } from "vitest";
 
 describe("timerIsAboutToEnd", () => {
-  it("should be true when the argument is <= 10 but > 0", () => {
+  it("should be true when the argument is greater than 10 but less than 0", () => {
     expect(timerIsAboutToEnd(10)).toBeTruthy();
     expect(timerIsAboutToEnd(4)).toBeTruthy();
   });
 
-  it("should be false when the argument is <= 0", () => {
+  it("should be false when the argument is less than or equal to 0", () => {
     expect(timerIsAboutToEnd(0)).toBeFalsy();
     expect(timerIsAboutToEnd(-1)).toBeFalsy();
   });
 
-  it("should be false when the argument is > 10", () => {
+  it("should be false when the argument is greater than 10", () => {
     expect(timerIsAboutToEnd(11)).toBeFalsy();
     expect(timerIsAboutToEnd(54)).toBeFalsy();
   });
 });
 
 describe("timerHasEnded", () => {
-  it("should be true when the argument is <= 0", () => {
+  it("should be true when the argument is less than or equal to 0", () => {
     expect(timerHasEnded(0)).toBeTruthy();
     expect(timerHasEnded(-5)).toBeTruthy();
   });
 
-  it("should be false when the argument is > 0", () => {
+  it("should be false when the argument is greater than 0", () => {
     expect(timerHasEnded(10)).toBeFalsy();
     expect(timerHasEnded(3)).toBeFalsy();
   });
