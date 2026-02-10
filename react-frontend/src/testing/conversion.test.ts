78e00cd6ff180c5ec454963ebee19e6daf77835a
Daniel Leith <elixerprince@gmail.com>
2026-02-09 21:41:33 -0500

    test(utils): create conversion test cases

--- react-frontend/src/testing/conversion.test.ts
+++ react-frontend/src/testing/conversion.test.ts
@@ -1,4 +1,10 @@
-import { convertMinutesToSeconds } from "@/utils/conversion";
+import {
+  convertMillisecondsToSeconds,
+  convertMinutesToMilliseconds,
+  convertMinutesToSeconds,
+  convertSecondsToMilliseconds,
+  convertSecondsToMinutes,
+} from "@/utils/conversion";
 
 describe("convertMinutesToSeconds", () => {
   it("should return 60 when given 1 as an argument", () => {
@@ -9,5 +15,45 @@ describe("convertMinutesToSeconds", () => {
   it("should return -60 when given -1 as an argument", () => {
     const result = convertMinutesToSeconds(-1);
     expect(result).toBe(-60);
+  });
+});
+
+describe("convertMinutesToMilliseconds", () => {
+  it("should return 60000 when given 1 as an argument", () => {
+    expect(convertMinutesToMilliseconds(1)).toBe(60000);
+  });
+
+  it("should return -60000 when given -1 as an argument", () => {
+    expect(convertMinutesToMilliseconds(-1)).toBe(-60000);
+  });
+});
+
+describe("convertSecondsToMinutes", () => {
+  it("should return 1 when given 60 as an argument", () => {
+    expect(convertSecondsToMinutes(60)).toBe(1);
+  });
+
+  it("should return -1 when given -60 as an argument", () => {
+    expect(convertSecondsToMinutes(-60)).toBe(-1);
+  });
+});
+
+describe("convertSecondsToMilliseconds", () => {
+  it("should return 1000 when given 1 as an argument", () => {
+    expect(convertSecondsToMilliseconds(1)).toBe(1000);
+  });
+
+  it("should return -1000 when given -1 as an argument", () => {
+    expect(convertSecondsToMilliseconds(-1)).toBe(-1000);
+  });
+});
+
+describe("convertMillisecondsToSeconds", () => {
+  it("should return 1 when given 1000 as an argument", () => {
+    expect(convertMillisecondsToSeconds(1000)).toBe(1);
+  });
+
+  it("should return -1 when given -1000 as an argument", () => {
+    expect(convertMillisecondsToSeconds(-1000)).toBe(-1);
   });
 });
