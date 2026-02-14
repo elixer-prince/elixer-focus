be283c417e5969966a5a88b888e4a224b743543e
Daniel Leith <elixerprince@gmail.com>
2026-02-14 04:21:14 -0500

    test(utils): create a dummy test
    I am still learning how to use Vitest, so the file won't be populated as yet

---
+++ react-frontend/src/testing/utils/storage.test.ts
@@ -0,0 +1,7 @@
+import { it, expect, describe } from "vitest";
+
+describe("getFromLocalStorage", () => {
+  it("should return the stored item when given a valid key", () => {
+    expect(true).toBe("true");
+  });
+});
