eaec6696ab5c59388416fe85bddda196fe5e82f7
Daniel Leith <elixerprince@gmail.com>
2026-02-14 13:18:36 -0500

    fix(navbar): correct inner wrapper container alignment
    The navbar inner wrapper container was at the center of the navbar whe the navbar was opened.

--- web/src/components/navigation/Navbar/Index.tsx
+++ web/src/components/navigation/Navbar/Index.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`navbar border-b-base-content/50 fixed top-0 right-0 left-0 z-20 h-(--navbar-height) border-b p-4 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
+      className={`navbar border-b-base-content/50 fixed top-0 right-0 left-0 z-20 h-(--navbar-height) items-start border-b p-4 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
     >
       <div className="wrapper-container flex h-10 w-full items-center justify-between">
         <div className={"inner-left-container flex items-center gap-2"}>
