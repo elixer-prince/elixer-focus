076d12d27afc2fc86c7adccdbe94da18d0c1a91f
Daniel Leith <elixerprince@gmail.com>
2026-02-14 17:17:41 -0500

    feat(navbar): add alpha text to the site logo
    This is to make the alpha status obvious to anyone visiting the site and to myself mentally.

--- web/src/components/navigation/NavLogo.tsx
+++ web/src/components/navigation/NavLogo.tsx
@@ -4,7 +4,7 @@ const NavLogo = ({ className }: { className?: string }) => {
   return (
     <Link to={"/"} className={`${className} select-none`.trim()}>
       <span className={"inline-block text-xl font-bold"}>
-        Elixer Focus <sub className={"text-[0.5rem]"}>TM</sub>
+        Elixer Focus (ALPHA)<sub className={"text-[0.5rem]"}>TM</sub>
       </span>
     </Link>
   );
