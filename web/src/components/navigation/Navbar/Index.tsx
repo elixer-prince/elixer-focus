a8cf7f6ad3310bb3e5605752f295a0fccdf15ac5
Daniel Leith <elixerprince@gmail.com>
2026-02-14 12:35:44 -0500

    style(navbar): remove component comments in favour of class names
    This will reduce the over lines in the file and is easier to read with the Tailwind Fold with Inline extension.
    
    This extension only shows the first class name which can be used as documentation to describe what the following classes are styling.

--- web/src/components/navigation/Navbar/Index.tsx
+++ web/src/components/navigation/Navbar/Index.tsx
@@ -7,21 +7,20 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`border-b-base-content/50 fixed top-0 right-0 left-0 z-20 h-(--navbar-height) border-b p-4 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
+      className={`navbar border-b-base-content/50 fixed top-0 right-0 left-0 z-20 h-(--navbar-height) border-b p-4 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
     >
-      {/* Wrapper Container */}
-      <div className={"flex h-10 w-full items-center justify-between"}>
-        {/* Inner Left Container */}
-        <div className={"flex items-center gap-2"}>
+      <div className="wrapper-container flex h-10 w-full items-center justify-between">
+        <div className={"inner-left-container flex items-center gap-2"}>
           <HamburgerMenu />
 
-          <div className={"flex items-baseline gap-8"}>
+          <div className={"nav-logo-container flex items-baseline gap-8"}>
             <NavLogo />
           </div>
         </div>
 
-        {/* Inner Right Container */}
-        <div className={"flex items-center gap-4"}>{/* <LoginLink /> */}</div>
+        <div className={"inner-right-container flex items-center gap-4"}>
+          {/* <LoginLink /> */}
+        </div>
       </div>
     </nav>
   );
