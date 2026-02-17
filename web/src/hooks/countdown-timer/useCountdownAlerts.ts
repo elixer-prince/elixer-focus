1be761e916132379312fe1ef0d3e691571b1bda4
Daniel Leith <elixerprince@gmail.com>
2026-02-17 03:41:57 -0500

    chore(countdown-timer): remove toast component form alert function
    The toast component is no longer needed because I will implement a modal instead.

--- web/src/hooks/countdown-timer/useCountdownAlerts.ts
+++ web/src/hooks/countdown-timer/useCountdownAlerts.ts
@@ -4,14 +4,7 @@ import toast from "react-hot-toast";
 const useCountdownAlerts = () => {
   const currentSessionType = useCurrentSessionType();
 
-  // * Temp Locked * //
-  const alertUserOfTimerEnd = () => {
-    // TODO: Make this toast themed
-    // ? I might convert this to a overlay
-    toast(`Your ${currentSessionType} session has ended!`, {
-      duration: 10000,
-    });
-  };
+  const alertUserOfTimerEnd = () => {};
 
   return { alertUserOfTimerEnd };
 };
