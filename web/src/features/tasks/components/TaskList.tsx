6737b88536fda5552511ab1b4e5e97a22e09b834
Daniel Leith <elixerprince@gmail.com>
2026-02-21 13:20:59 -0500

    fix(tasks): fix handle drag end function

--- web/src/features/tasks/components/TaskList.tsx
+++ web/src/features/tasks/components/TaskList.tsx
@@ -1,18 +1,21 @@
 import Task from "@/features/tasks/components/Task";
 import { useSetTasks, useTasks } from "@/features/tasks/stores/tasks-store";
+import { move } from "@dnd-kit/helpers";
 import { DragDropProvider, type DragEndEvent } from "@dnd-kit/react";
 
 const TaskList = () => {
   const tasks = useTasks();
-  // const setTasks = useSetTasks();
+  const setTasks = useSetTasks();
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    // if (event.canceled) return;
-    // setTasks((items) => move(items, event));
+  const handleDragEnd: Parameters<typeof DragDropProvider>[0]["onDragEnd"] = (
+    event,
+  ) => {
+    const nextTasks = move(tasks, event);
+    setTasks(nextTasks);
   };
 
   return (
-    <DragDropProvider onDragEnd={() => handleDragEnd}>
+    <DragDropProvider onDragEnd={handleDragEnd}>
       <ul>
         {tasks.map(({ id, title, description }, index) => (
           <Task
