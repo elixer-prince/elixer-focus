d63e77c154eb63544a9cce67a4e3ef10ef46918d
Daniel Leith <elixerprince@gmail.com>
2026-02-20 21:14:46 -0500

    feat(routes): implement basic tasks page

--- web/src/app/routes/(root)/Tasks.tsx
+++ web/src/app/routes/(root)/Tasks.tsx
@@ -1,3 +1,5 @@
+import TaskList from "@/features/tasks/components/TaskList";
+import { useAddTask } from "@/features/tasks/stores/tasks-store";
 import usePageTitle from "@/hooks/usePageTitle";
 import { useTimerRunning } from "@/stores/countdown-timer/store";
 
@@ -7,7 +9,31 @@ const Tasks = () => {
 
   if (!timerRunning) updatePageTitle("Elixer Focus - Tasks");
 
-  return <>{/*<p>tasks Page...</p>*/}</>;
+  const addTask = useAddTask();
+
+  return (
+    <div className="p-12">
+      <div>
+        <h1 className="page-title mb-4 text-center text-4xl font-bold">
+          My Tasks
+        </h1>
+
+        <input
+          type="text"
+          className="input placeholder:text-primary mx-auto block placeholder:italic"
+          placeholder="Do the laundry..."
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              addTask(event.currentTarget.value);
+              event.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+
+      <TaskList />
+    </div>
+  );
 };
 
 export default Tasks;
