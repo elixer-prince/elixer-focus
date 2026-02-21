fa4a8d71866248af96a4a33cef350526157f2613
Daniel Leith <elixerprince@gmail.com>
2026-02-20 21:48:52 -0500

    feat(tasks): add delete button for each task

--- web/src/features/tasks/components/TaskList.tsx
+++ web/src/features/tasks/components/TaskList.tsx
@@ -1,12 +1,21 @@
-import { useTasks } from "../stores/tasks-store";
+import { useRemoveTask, useTasks } from "@/features/tasks/stores/tasks-store";
 
 const TaskList = () => {
   const tasks = useTasks();
+  const removeTask = useRemoveTask();
 
   return (
     <ul>
       {tasks.map((task) => (
-        <li key={task}>{task}</li>
+        <li key={task} className="flex items-center gap-2">
+          <p>{task}</p>
+          <button
+            className="text-error cursor-pointer hover:underline"
+            onClick={() => removeTask}
+          >
+            Remove
+          </button>
+        </li>
       ))}
     </ul>
   );
