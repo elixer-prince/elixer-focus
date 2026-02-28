49fb4e2d072650d31bd1d6c4e59f42256cf4a9e4
Daniel Leith <elixerprince@gmail.com>
2026-02-28 14:40:25 -0500

    chore(tasks): uncomment dnd function

--- web/src/app/routes/(root)/Tasks.tsx
+++ web/src/app/routes/(root)/Tasks.tsx
@@ -1,12 +1,10 @@
 import Column from "@/features/tasks/components/Column";
 import TaskHeader from "@/features/tasks/components/TaskHeader";
-import {} from // useSetTasks,
-// useTasks,
-"@/features/tasks/stores/tasks-store";
+import { useSetTasks, useTasks } from "@/features/tasks/stores/tasks-store";
 import usePageTitle from "@/hooks/usePageTitle";
 import { useTimerRunning } from "@/stores/countdown-timer/store";
-// import { move } from "@dnd-kit/helpers";
-// import { DragDropProvider } from "@dnd-kit/react";
+import { move } from "@dnd-kit/helpers";
+import { DragDropProvider } from "@dnd-kit/react";
 
 const Tasks = () => {
   const { updatePageTitle } = usePageTitle();
@@ -14,45 +12,45 @@ const Tasks = () => {
 
   if (!timerRunning) updatePageTitle("Elixer Focus - Tasks");
 
-  // const tasks = useTasks();
-  // const setTasks = useSetTasks();
+  const tasks = useTasks();
+  const setTasks = useSetTasks();
 
-  // const handleDragEnd: Parameters<typeof DragDropProvider>[0]["onDragEnd"] = (
-  //   event,
-  // ) => {
-  //   const nextTasks = move(tasks, event);
-  //   setTasks(nextTasks);
-  // };
+  const handleDragEnd: Parameters<typeof DragDropProvider>[0]["onDragEnd"] = (
+    event,
+  ) => {
+    const nextTasks = move(tasks, event);
+    setTasks(nextTasks);
+  };
 
   return (
     <div className="p-12">
       <TaskHeader />
 
-      <div className="tasks-container mt-8 flex flex-col items-center gap-8">
-        {/* <DragDropProvider onDragEnd={handleDragEnd}> */}
-        <div className="tasks-row flex gap-8">
-          <Column
-            title="Urgent and Important (Do First)"
-            category="urgent-important"
-          />
-          <Column
-            title="Not Urgent but Important (Schedule)"
-            category="not-urgent-important"
-          />
+      <DragDropProvider onDragEnd={handleDragEnd}>
+        <div className="tasks-container mt-8 flex flex-col items-center gap-8">
+          <div className="tasks-row flex gap-8">
+            <Column
+              title="Urgent and Important (Do First)"
+              category="urgent-important"
+            />
+            <Column
+              title="Not Urgent but Important (Schedule)"
+              category="not-urgent-important"
+            />
+          </div>
+          <div className="tasks-row flex gap-8">
+            <Column
+              title="Urgent but Not Important (Delegate)"
+              category="urgent-not-important"
+            />
+            <Column
+              title="Not Urgent nor Important (Eliminate)"
+              category="not-urgent-not-important"
+            />
+          </div>
+          <Column title="Uncategorised" category="uncategorised" />
         </div>
-        <div className="tasks-row flex gap-8">
-          <Column
-            title="Urgent but Not Important (Delegate)"
-            category="urgent-not-important"
-          />
-          <Column
-            title="Not Urgent nor Important (Eliminate)"
-            category="not-urgent-not-important"
-          />
-        </div>
-        <Column title="Uncategorised" category="uncategorised" />
-        {/* </DragDropProvider> */}
-      </div>
+      </DragDropProvider>
     </div>
   );
 };
