ded9eb8accfa114b75a3c5740a6fff004fc12a4b
Daniel Leith <elixerprince@gmail.com>
2026-03-02 12:47:10 -0500

    fix(navbar): update current tim every second

--- web/src/features/ui/navigation/Navbar/CurrentTime.tsx
+++ web/src/features/ui/navigation/Navbar/CurrentTime.tsx
@@ -1,10 +1,20 @@
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 
 const CurrentTime = () => {
-  const currentTime = dayjs().format("h:mm A");
+  const [currentTime, setCurrentTime] = useState(dayjs().format("h:mm:ss A"));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(dayjs().format("h:mm:ss A"));
+    }, 1000);
+
+    // Cleanup when component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="current-time border-primary/25 bg-base-100 rounded-md border-2 p-2 font-bold shadow-md max-sm:hidden">
+    <div className="current-time hover:bg-base-content/10 border-primary/25 bg-base-100 rounded-md border-2 p-2 font-bold shadow-md transition-colors duration-500 select-none max-sm:hidden">
       {currentTime}
     </div>
   );
