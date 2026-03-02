39afbd3be8fb65e969e5c78907a48f8cf626e393
Daniel Leith <elixerprince@gmail.com>
2026-03-02 14:24:59 -0500

    fix(countdown-timer): early return if current session count is zero

--- web/src/features/countdown-timer/components/TimerDisplay/SessionCount.tsx
+++ web/src/features/countdown-timer/components/TimerDisplay/SessionCount.tsx
@@ -15,6 +15,7 @@ const SessionCount = () => {
   return (
     <button
       onClick={() => {
+        if (currentSessionCount === 0) return;
         if (confirm("Are you sure you want to reset the session count?")) {
           resetCurrentSessionCount();
         }
