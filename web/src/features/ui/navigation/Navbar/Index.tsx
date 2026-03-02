18af7acee2e195e0d279290854c6fcda1ec94601
Daniel Leith <elixerprince@gmail.com>
2026-03-02 18:35:35 -0500

    style(navbar): remove unnecessary braces

--- web/src/features/ui/navigation/Navbar/Index.tsx
+++ web/src/features/ui/navigation/Navbar/Index.tsx
@@ -11,15 +11,15 @@ const Navbar = () => {
       className={`navbar border-b-base-content/50 fixed top-0 right-0 left-0 z-40 h-(--navbar-height) items-start border-b p-4 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
     >
       <div className="wrapper-container flex h-10 w-full items-center justify-between">
-        <div className={"inner-left-container flex items-center gap-2"}>
+        <div className="inner-left-container flex items-center gap-2">
           <HamburgerMenu />
 
-          <div className={"nav-logo-container flex items-baseline gap-8"}>
+          <div className="nav-logo-container flex items-baseline gap-8">
             <NavLogo />
           </div>
         </div>
 
-        <div className={"inner-right-container flex items-center gap-4"}>
+        <div className="inner-right-container flex items-center gap-4">
           {/* <LoginLink /> */}
           <CurrentTime />
         </div>
