14f302a0eb3891bfa44a3a311f626d6c2d77c42a
Daniel Leith <elixerprince@gmail.com>
2026-03-02 00:34:30 -0500

    feat(tasks): allow user to add tasks to specific categories

--- web/src/features/tasks/components/Column/TaskList.tsx
+++ web/src/features/tasks/components/Column/TaskList.tsx
@@ -1,6 +1,6 @@
 import Ghost from "@/features/tasks/components/Task/Ghost";
 import Task from "@/features/tasks/components/Task/Index";
-import { useTasks } from "@/features/tasks/stores/tasks-store";
+import { useAddTask, useTasks } from "@/features/tasks/stores/tasks-store";
 import type { TaskCategory } from "@/features/tasks/types/task";
 
 interface TaskListProps {
@@ -13,6 +13,8 @@ const TaskList = ({ category, inputShown, placeholder }: TaskListProps) => {
   const allTasks = useTasks();
   const tasks = allTasks.filter((task) => task.category === category);
 
+  const addTask = useAddTask();
+
   return (
     <ul className="tasks-container__task-list flex flex-col gap-4 overflow-auto pt-1 pr-1 pb-4 pl-4">
       <div className={tasks.length === 0 ? "mr-3 space-y-4" : "hidden"}>
@@ -21,6 +23,16 @@ const TaskList = ({ category, inputShown, placeholder }: TaskListProps) => {
             type="text"
             placeholder={placeholder}
             className="task-input input border-primary-content/25 input-primary w-full placeholder:italic"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                if (event.currentTarget.value.trim() === "") {
+                  event.currentTarget.value = "";
+                  return;
+                }
+                addTask(event.currentTarget.value, "", category);
+                event.currentTarget.value = "";
+              }
+            }}
           />
         )}
 
