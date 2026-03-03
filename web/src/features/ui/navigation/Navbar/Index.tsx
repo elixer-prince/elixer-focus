e2aadb3700f283de1d29782b5fa2a78787d8e7f5
Daniel Leith <elixerprince@gmail.com>
2026-03-02 19:54:45 -0500

    feat(navbar): add mobile navbar links
    - increase navbar height
    - create navbar link and container components
    - create use navbar hook
    - create open and close navbar functions

--- web/src/features/ui/navigation/Navbar/Index.tsx
+++ web/src/features/ui/navigation/Navbar/Index.tsx
@@ -1,5 +1,6 @@
 import CurrentTime from "@/features/ui/navigation/Navbar/CurrentTime";
 import HamburgerMenu from "@/features/ui/navigation/Navbar/HamburgerMenu";
+import MobileLinkContainer from "@/features/ui/navigation/Navbar/MobileLinkContainer";
 import { useNavbarIsOpen } from "@/features/ui/navigation/Navbar/stores/NavbarStore";
 import NavLogo from "@/features/ui/navigation/NavLogo";
 
@@ -8,9 +9,9 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`navbar border-b-base-content/50 fixed top-0 right-0 left-0 z-40 h-(--navbar-height) items-start border-b p-4 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
+      className={`navbar border-b-base-content/50 fixed top-0 right-0 left-0 z-40 flex h-(--navbar-height) flex-col items-start border-b p-0 shadow-md backdrop-blur-sm transition-all duration-1000 ${navbarIsOpen ? "max-md:bg-base-300 max-md:h-screen" : ""}`.trim()}
     >
-      <div className="wrapper-container flex h-10 w-full items-center justify-between">
+      <div className="wrapper-container flex min-h-19.5 w-full items-center justify-between p-4">
         <div className="inner-left-container flex items-center gap-2">
           <HamburgerMenu />
 
@@ -23,6 +24,10 @@ const Navbar = () => {
           {/* <LoginLink /> */}
           <CurrentTime />
         </div>
+      </div>
+
+      <div className="flex size-full justify-center p-4 md:hidden">
+        {navbarIsOpen && <MobileLinkContainer />}
       </div>
     </nav>
   );
