17e43937f9099051e600500bef04593c524eac3b
Daniel Leith <elixerprince@gmail.com>
2026-03-07 01:12:05 -0500

    test(testing): add global resize observer mock to remove errors

--- web/src/testing/setup.ts
+++ web/src/testing/setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom/vitest";
+
+class ResizeObserver {
+  observe = vi.fn();
+  unobserve = vi.fn();
+  disconnect = vi.fn();
+}
+
+globalThis.ResizeObserver = ResizeObserver;
