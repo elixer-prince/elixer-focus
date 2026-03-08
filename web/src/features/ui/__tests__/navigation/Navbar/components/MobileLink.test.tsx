36b9c32a11c3de5c5ec2d8060ff76e4757f084dd
Daniel Leith <elixerprince@gmail.com>
2026-03-08 10:49:29 -0500

    test(Navbar): write test for mobile link component

---
+++ web/src/features/ui/__tests__/navigation/Navbar/components/MobileLink.test.tsx
@@ -0,0 +1,40 @@
+import MobileLink from "@/features/ui/navigation/Navbar/components/MobileLink";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router";
+import userEvent from "@testing-library/user-event";
+
+const CurrentPath = () => {
+  const location = useLocation();
+  return <span data-testid="current-path">{location.pathname}</span>;
+};
+
+// Tests
+
+describe("Mobile Link", () => {
+  it("should render a link element", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <MobileLink to="/pathname">Path Name</MobileLink>
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByRole("link")).toBeInTheDocument();
+  });
+
+  it("should navigate to the login page when clicked", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <MobileLink to="/pathname">Path Name</MobileLink>
+        <Routes>
+          <Route path="*" element={<CurrentPath />} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const loginLink = screen.getByRole("link");
+    await userEvent.click(loginLink);
+
+    const currentPath = screen.getByTestId("current-path");
+    expect(currentPath.textContent).toBe("/pathname");
+  });
+});
