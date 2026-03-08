aa68b78cc8965b0ddb0c43c815ec1b74076af34e
Daniel Leith <elixerprince@gmail.com>
2026-03-08 01:12:18 -0500

    feat(Navbar): change div to article and add aria-label for accessibility

--- web/src/features/ui/navigation/Navbar/components/CurrentTime.tsx
+++ web/src/features/ui/navigation/Navbar/components/CurrentTime.tsx
@@ -4,9 +4,12 @@ const CurrentTime = () => {
   const currentTime = useCurrentTime();
 
   return (
-    <div className="current-time hover:bg-base-content/10 border-primary/25 bg-base-100 rounded-md border-2 p-2 font-bold shadow-md transition-colors duration-500 select-none max-sm:hidden">
+    <article
+      aria-label="Current time display"
+      className="current-time hover:bg-base-content/10 border-primary/25 bg-base-100 rounded-md border-2 p-2 font-bold shadow-md transition-colors duration-500 select-none max-sm:hidden"
+    >
       {currentTime}
-    </div>
+    </article>
   );
 };
 
