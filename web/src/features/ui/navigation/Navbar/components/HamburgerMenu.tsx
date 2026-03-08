2a4199e0957978378c95102dea564468c736e185
Daniel Leith <elixerprince@gmail.com>
2026-03-07 20:55:20 -0500

    fix(Navbar): move on click handler to button instead of icon

--- web/src/features/ui/navigation/Navbar/components/HamburgerMenu.tsx
+++ web/src/features/ui/navigation/Navbar/components/HamburgerMenu.tsx
@@ -5,8 +5,11 @@ const HamburgerMenu = () => {
   const toggleNavbar = useToggleNavbar();
 
   return (
-    <button className="hamburger-menu hover:bg-base-content/5 outline-primary cursor-pointer rounded-md p-2 transition-colors active:outline md:hidden">
-      <MdMenu size={24} onClick={toggleNavbar} />
+    <button
+      onClick={toggleNavbar}
+      className="hamburger-menu hover:bg-base-content/5 outline-primary cursor-pointer rounded-md p-2 transition-colors active:outline md:hidden"
+    >
+      <MdMenu size={24} />
     </button>
   );
 };
