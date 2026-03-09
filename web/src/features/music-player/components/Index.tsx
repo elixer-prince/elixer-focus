761c5bdf5732463c2f8ded007a1bebc7d52f1165
Daniel Leith <elixerprince@gmail.com>
2026-03-09 16:52:40 -0500

    style(music-player): convert semantic classname to comments

--- web/src/features/music-player/components/Index.tsx
+++ web/src/features/music-player/components/Index.tsx
@@ -13,8 +13,10 @@ const MusicPlayer = () => {
   useMusicPlayer({ playerRef, playerInstanceRef });
 
   return (
-    <article className="music-player border-t-base-content/25 bg-base-100 hover:outline-primary/75 fixed right-4 bottom-5 z-30 flex h-(--music-player-height) items-center gap-4 rounded-xl border-t-2 p-4 shadow-lg outline-2 outline-transparent transition-all duration-300 select-none hover:-translate-y-0.5">
-      <div className="invisible-player hidden" ref={playerRef}></div>
+    // Music Player
+    <article className="debug-border border-t-base-content/25 bg-base-100 hover:outline-primary/75 fixed right-4 bottom-5 z-30 flex h-(--music-player-height) items-center gap-4 rounded-xl border-t-2 p-4 shadow-lg outline-2 outline-transparent transition-all duration-300 select-none hover:-translate-y-0.5">
+      {/* Invisible Player */}
+      <div className="hidden" ref={playerRef}></div>
 
       <MusicSwitcher />
 
