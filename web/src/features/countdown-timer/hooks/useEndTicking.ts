407330313ca7f7f8edce77252ac1b2a26c0242f0
Daniel Leith <elixerprince@gmail.com>
2026-03-14 23:36:43 -0500

    chore(countdown-timer): remove unnecessary jsdoc comments

--- web/src/features/countdown-timer/hooks/useEndTicking.ts
+++ web/src/features/countdown-timer/hooks/useEndTicking.ts
@@ -5,12 +5,6 @@ import { useCallback } from "react";
 const useEndTicking = () => {
   const { timerTickingSoundEffectRef, isEndTickingRef } = useCountdownContext();
 
-  // * Locked * //
-  /**
-   * Start the timer ticking sound effect if it is not already ticking.
-   *
-   * @returns {void}
-   */
   const startEndTicking = useCallback((): void => {
     if (isEndTickingRef.current) return;
 
@@ -27,12 +21,6 @@ const useEndTicking = () => {
     playSound(sound);
   }, [isEndTickingRef, timerTickingSoundEffectRef]);
 
-  // * Locked * //
-  /**
-   * Stop the timer ticking sound effect if it hasn't started yet.
-   *
-   * @returns {void}
-   */
   const stopEndTicking = useCallback((): void => {
     if (!isEndTickingRef.current) return;
 
